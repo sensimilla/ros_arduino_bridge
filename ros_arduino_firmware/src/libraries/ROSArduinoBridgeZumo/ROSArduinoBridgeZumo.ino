348b4c42101f70376cd3897557ba1ea641e1fca9
sensimilla <pi@pibot2.cedars.dom>
2021-11-01 00:53:43 +0000

    updates to arduino firmware to mostly enable IMU

--- ros_arduino_firmware/src/libraries/ROSArduinoBridgeZumo/ROSArduinoBridgeZumo.ino
+++ ros_arduino_firmware/src/libraries/ROSArduinoBridgeZumo/ROSArduinoBridgeZumo.ino
@@ -124,6 +124,65 @@
 #ifdef USE_ZUMO
    #include <Wire.h>
    #include <ZumoShield.h>
+   #include "zumo_driver.h"  
+   /* Stop the robot if it hasn't received a movement command
+   in this number of milliseconds */
+   #define AUTO_STOP_INTERVAL 2000
+   long lastMotorCommand = AUTO_STOP_INTERVAL;
+   // Character array for imu values
+   char imu_report[120];
+#endif
+
+/* Initialise Zumo hardware if used */
+#ifdef USE_ZUMO
+  //Wire.begin();
+  ZumoBuzzer buzzer;
+  Pushbutton button(ZUMO_BUTTON);
+  //ZumoMotors motors;
+  ZumoIMU imu;
+
+  //constants necessary to convert the acceleromter's raw values into millimiters per second squared 
+  #define G_PER_LSB .000061
+  #define MSS_PER_G 9.80665
+
+  // magnetometer scaling LSB = least significant bit
+  #define MGAUSS_PER_LSB 0.160
+
+  // gyro scaling to degrees / second
+  #define DEGREES_PER_LSB 0.00875
+
+  #define ToRad(x) ((x)*0.01745329252)  // *pi/180
+
+  // LSM303/LIS3MDL magnetometer calibration constants; use the Calibrate example from
+  // the Pololu LSM303 or LIS3MDL library to find the right values for your board
+
+  #define M_X_MIN -2330
+  #define M_Y_MIN -1523
+  #define M_Z_MIN -16698
+  #define M_X_MAX +2028
+  #define M_Y_MAX +3040
+  #define M_Z_MAX -10790
+
+  float gx, gy, gz, ax, ay, az, mx, my, mz; 
+
+  // offsets for gyro and accelerometer
+  int offset_ax = -5;
+  int offset_ay = -83;
+  int offset_az = -10;
+
+  int offset_gx = -4;
+  int offset_gy = -1;
+  int offset_gz = -528;
+
+    // Uncomment the below line to use this axis definition:
+    // X axis pointing forward
+    // Y axis pointing to the right
+    // and Z axis pointing down.
+  // Positive pitch : nose up
+  // Positive roll : right wing down
+  // Positive yaw : clockwise
+
+  int SENSOR_SIGN[9] = {1,1,1,-1,-1,-1,1,1,1}; //Correct directions x,y,z - gyro, accelerometer, magnetometer
 #endif
 
 /* Variable initialization */
@@ -202,7 +261,6 @@ int runCommand() {
     Serial.println(servos[arg1].getServo().read());
     break;
 #endif
-    
 #ifdef USE_BASE
   case READ_ENCODERS:
     Serial.print(readEncoder(LEFT));
@@ -241,11 +299,107 @@ int runCommand() {
 #endif
 #ifdef USE_ZUMO
   case READ_BUTTON:
-    Serial.println(button.isPressed());
+    Serial.println(button.getSingleDebouncedRelease());
     break;
   case BUZZER_WRITE:
-    buzzer.play(F("l8 cdefgab>c"));
-    Serial.println("OK");
+    switch(arg1) {
+      case 1:
+        // bad sound
+        buzzer.play("l8 fefdecdd");
+        Serial.println("OK");
+        break;
+      default:
+        // happy sound
+        buzzer.play("l8 dbg");
+        Serial.println("OK");
+        break;
+    }
+    break;
+  case MOTOR_SPEEDS:
+    /* Reset the auto stop timer */
+    lastMotorCommand = millis();
+    if (arg1 == 0 && arg2 == 0) {
+      setMotorSpeeds(0, 0);
+    }
+    setMotorSpeed(LEFT, arg1);
+    setMotorSpeed(RIGHT, arg2);
+    Serial.println("OK"); 
+    break;
+  case READ_IMU:
+    imu.read();
+    
+    // convert gyro readings to radians per second
+
+    gx = imu.g.x + offset_gx;
+    gy = imu.g.y + offset_gy;
+    gz = imu.g.z + offset_gy;
+
+    // convert to degrees per second
+    gx = gx * DEGREES_PER_LSB;
+    gy = gy * DEGREES_PER_LSB;
+    gz = gz * DEGREES_PER_LSB;
+
+    gx = ToRad(gx);
+    gy = ToRad(gy);
+    gz = ToRad(gz);
+    
+    // convert acceleration to meters per second
+    ax = imu.a.x + offset_ax;
+    ay = imu.a.y + offset_ay;
+    az = imu.a.z + offset_az;
+
+    // convert to m/s
+    ax = ax * G_PER_LSB * MSS_PER_G;
+    ay = ay * G_PER_LSB * MSS_PER_G;
+    az = az * G_PER_LSB * MSS_PER_G;
+
+    // apply magnetic snsor calibration
+    // adjust for LSM303 compass axis offsets/sensitivity differences by scaling to +/-0.5 range
+    mx = (float)(imu.m.x - SENSOR_SIGN[6]*M_X_MIN) / (M_X_MAX - M_X_MIN) - SENSOR_SIGN[6]*0.5;
+    my = (float)(imu.m.y - SENSOR_SIGN[7]*M_Y_MIN) / (M_Y_MAX - M_Y_MIN) - SENSOR_SIGN[7]*0.5;
+    mz = (float)(imu.m.z - SENSOR_SIGN[8]*M_Z_MIN) / (M_Z_MAX - M_Z_MIN) - SENSOR_SIGN[8]*0.5; 
+
+    // convert to uT microTesla
+    mx = imu.m.x * MGAUSS_PER_LSB * 0.1;
+    my = imu.m.y * MGAUSS_PER_LSB * 0.1;
+    mz = imu.m.z * MGAUSS_PER_LSB * 0.1;
+
+    Serial.print(gx);
+    Serial.print(" ");
+    Serial.print(gy);
+    Serial.print(" ");
+    Serial.print(gz);
+    Serial.print(" ");
+    Serial.print(ax);
+    Serial.print(" ");
+    Serial.print(ay);
+    Serial.print(" ");
+    Serial.print(az);
+    Serial.print(" ");
+    Serial.print(mx);
+    Serial.print(" ");
+    Serial.print(my);
+    Serial.print(" ");
+    Serial.println(mz);       
+    
+
+    // Serial.print(imu.g.x);
+    // Serial.print(" ");
+    // Serial.print(imu.g.y);
+    // Serial.print(" ");
+    // Serial.print(imu.g.z);
+    // Serial.print(" ");
+    // Serial.print(imu.a.x);
+    // Serial.print(" ");
+    // Serial.print(imu.g.y);
+    // Serial.print(" ");
+    // Serial.print(imu.g.z);
+    // Serial.print(" ");
+    // Serial.print(imu.m.x);
+    // Serial.print(" ");
+    // Serial.print(imu.m.y);
+    // Serial.print(" ");
+    // Serial.println(imu.m.z);    
     break;
 #endif
   default:
@@ -295,16 +449,18 @@ void setup() {
           servoInitPosition[i]);
     }
   #endif
-
-/* Initialise Zumo hardware if used */
   #ifdef USE_ZUMO
     Wire.begin();
-    ZumoBuzzer buzzer;
-    Pushbutton button(ZUMO_BUTTON);
-    ZumoIMU imu;
+    if (!imu.init()) {
+        // Failed to detect the compass.
+        while(1) {
+         Serial.println(F("Failed to initialize IMU sensors."));
+         delay(100);
+         }
+      }
+    imu.enableDefault();
   #endif
 }
-
 /* Enter the main loop.  Read and parse input from the serial port
    and run any valid commands. Run a PID calculation at the target
    interval and check for auto-stop conditions.
@@ -371,5 +527,12 @@ void loop() {
     servos[i].doSweep();
   }
 #endif
-}
 
+#ifdef USE_ZUMO
+  // Check to see if we have exceeded the auto-stop interval
+  if ((millis() - lastMotorCommand) > AUTO_STOP_INTERVAL) {
+    setMotorSpeeds(0, 0);
+    //moving = 0;
+  }
+#endif  
+}
